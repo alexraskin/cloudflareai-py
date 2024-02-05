7bbada64c191c4386d0102295c24f26166035754
alexraskin <alexraskin@fastmail.fm>
2024-02-05 13:52:08 -0700

    bump

--- CloudflareAI/__init__.py
+++ CloudflareAI/__init__.py
@@ -5,14 +5,14 @@ Cloudflare AI Python SDK
 __title__ = "cloudflareai"
 __author__ = "alexraskin"
 __license__ = "Mozilla Public License 2.0"
-__version__ = "0.3.0"
+__version__ = "0.4.0"
 
 from .core import CloudflareAI
 from .enums import (
-  AiImageClassificationModels,
-  AiTranslationModels,
-  AiTextGenerationModels,
-  AiSpeechRecognitionModels,
-  AiTextToImageModels,
-  TranslationLanguages
+    AiImageClassificationModels,
+    AiTranslationModels,
+    AiTextGenerationModels,
+    AiSpeechRecognitionModels,
+    AiTextToImageModels,
+    TranslationLanguages,
 )
